fe8562aad72e33608677c485686140e9ac97b51b
Santisteban <san21153@uvg.edu.gt>
2024-07-31 08:36:39 -0600

    cambios en main

--- main.py
+++ main.py
@@ -8,23 +8,38 @@ def send_receive(message, port):
         return s.recv(1024).decode()
 
 def main():
+
+    print('-------------------Bienvenido al programa de codificación de mensajes-------------------\n')
+    
+    salir = False
+    while not salir:
+        print('Por favor ingrese el metodo de codificación de su mensaje')
+        print('1. Hamming (corrector de errores)')
+        print('2. CRC-32 (detector de errores)')
+        print('3. Salir\n')
+        op = input ('Ingrese el número de la opción deseada: ')
+        if op == '1':
+            print('-------------------Codificación Hamming-------------------\n\n')
+            # Esperar a que los servidores estén listos
+            input("Presiona Enter cuando los servidores estén listos...")
+            # Obtener el mensaje del usuario
+            message = input("Introduce un mensaje para codificar: ")
+            # Enviar al emisor y recibir la codificación
+            encoded = send_receive(message, 5000)
+            print("Mensaje codificado:", encoded)
+            # Enviar al receptor y recibir la decodificación
+            decoded = send_receive(encoded, 5001)
+            print("Mensaje decodificado:", decoded)
+        elif op == '2':
+            #pending...
+            pass
+        elif op == '3':
+            salir = True
+        else:
+            print('Opción no válida')
     # Iniciar el emisor y receptor
-    subprocess.Popen(["python", "Hamming/Hamming.py"])
-    subprocess.Popen(["go", "run", "Hamming/Hamming.go"])
 
-    # Esperar a que los servidores estén listos
-    input("Presiona Enter cuando los servidores estén listos...")
-
-    # Obtener el mensaje del usuario
-    message = input("Introduce un mensaje para codificar: ")
-
-    # Enviar al emisor y recibir la codificación
-    encoded = send_receive(message, 5000)
-    print("Mensaje codificado:", encoded)
-
-    # Enviar al receptor y recibir la decodificación
-    decoded = send_receive(encoded, 5001)
-    print("Mensaje decodificado:", decoded)
+    
 
 if __name__ == "__main__":
     main()
